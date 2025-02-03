645f96f113b07e502bf6efe4159b6b78f2f16240
Emilia Schrøder <emsc0004@stud.kea.dk>
2025-02-03 13:55:26 +0100

    done dynamisk produktliste

---
+++ list.js
@@ -0,0 +1,29 @@
+const listContainer = document.querySelector(".container");
+
+// Hver gang man fetcher henter man data, og man skal have de tre flg linjer:
+fetch(`https://kea-alt-del.dk/t7/api/products/`)
+  .then((response) => response.json())
+  .then((data) => showList(data));
+
+// Jeg har bedt min data om at hente produkterne
+function showList(products) {
+  const markup = products
+    // For hvert produkt putter jeg et nyt produkt ind der hedder "markup"
+    .map(
+      (product) => `
+<div class="box box-1">
+        <div class="image-container">
+          <a href="produkt.html">
+          <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="product image" /></a>
+          <p class="sold-out">Sold out</p>
+        </div>
+        <h4 class="opacity">${product.productdisplayname}</h4>
+        <p class="opacity">${product.category} | ${product.articletype}</p>
+        <p class="opacity">${product.price} kr.</p>
+        <a href="produkt.html"> <p class="click opacity">Read more</p></a>
+      </div>
+    `
+    )
+    .join("");
+  listContainer.innerHTML = markup;
+}
--- single.js
+++ single.js
@@ -1,21 +1,20 @@
 let productContainer = document.querySelector(".productContainer");
 // Tag fat i den rigtige container class^
+// Produkt id variabel
 let productId = 1577;
 
+// Henter JSON fil og får adgang til dens data:
 fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
+  // Den har hentet filen, og gør det om til gyldig json
   .then((response) => response.json())
   // Istedet for at skrive produkt nummer, kan man indsætte variablen istedet for.
 
-  // . betyder en metode der kommer i forlængelse af noget
-  // Jeg har kaldt min variabel "data"
-  // Fjerne html fra filen, og definere med javascript istedet for. Så jeg kan bestemme hvad der skal stå i template.
-  // Alt inde i en container skal fjernes, container skal stadig være der så jeg har noget at referere til.
-
+  // Derefter henter den data fra json filen
   .then((data) => {
     productContainer.innerHTML = `
       <img
         src="https://kea-alt-del.dk/t7/images/webp/640/${productId}.webp"
-        alt="${data.productdisplayname}"
+        alt="grøn cap"
       />
       <div class="pad-top">
         <h5> ${data.brandname} | ${data.category}</h5>
@@ -31,3 +30,8 @@ fetch(`https://kea-alt-del.dk/t7/api/products/${productId}`)
       </div>
     `;
   });
+
+// . betyder en metode der kommer i forlængelse af noget
+// Jeg har kaldt min variabel "data"
+// Fjerne html fra filen, og definere med javascript istedet for. Så jeg kan bestemme hvad der skal stå i template.
+// Alt inde i en container skal fjernes, container skal stadig være der så jeg har noget at referere til.
